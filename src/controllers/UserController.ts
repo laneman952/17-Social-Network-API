cd20514600a13e730e7f596c7e1efced84fdb6c1
Kyle Code <kyle.lane1992@yahoo.com>
2025-03-06 17:44:53 -0500

    updated userController and thoughtController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,1 +1,19 @@
-import  { Request, Response } from 'express';
+import  { Router } from 'express';
+const router = Router();
+import {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} from '../../controllers/userController.ts';
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+export { router as userRouter };
---
+++ src/controllers/thoughtController.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+const router = Router();
+import {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} from '../../controllers/thoughtController.ts';
+
+router.route('/').get(getAllThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+export { router as thoughtRouter };
