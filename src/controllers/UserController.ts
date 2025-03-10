bd78266f2bace4b7fcc89f9e37f6e30218111bb6
Kyle Code <kyle.lane1992@yahoo.com>
2025-03-10 19:20:17 -0400

    updated userController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from 'express';
 import { ObjectId } from 'mongodb';
-import { User } from '../models/User';
+import User from '../models/User';
 
-export const getAllUsers = async () => {
-    const 
-}
+export const getAllUsers = async (_req: Request, res: Response) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching users", error });
+    }
+};
